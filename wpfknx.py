7e8da066e3c5e1c67926b495535864b615a24b1f
jcoenencom <jpcoenen@gmail.com>
2024-08-01 18:22:31 +0200

    Update wpfknx.py
    INsert knxgtw config in class, re-connecct knxd if socket closed

--- wpfknx.py
+++ wpfknx.py
@@ -4,6 +4,7 @@ import asyncio
 import knx
 import __main__
 from asgiref.sync import async_to_sync
+from knx import connect
 
 
 class wpf:
@@ -14,7 +15,7 @@ class wpf:
 ### object.print() will print its definition
 
         
-        def __init__(self,xknx,nom,grpaddr,dpt,candest,unit,bus):
+        def __init__(self,xknx,nom,grpaddr,dpt,candest,unit,bus,knxgtw):
                 self.xknx=xknx          # xknx instance
                 self.nom = nom          # name of the object eg T Source
                 self.grpaddr=grpaddr    # corresponding KNX group address
@@ -23,6 +24,7 @@ class wpf:
                 self.unit=unit          # can unit number, appears in front of fa byte in request
                 self.val=0              # value read from can bus and sent to KNX bus
                 self.bus=bus            # can bus
+                self.knxgtw=knxgtw
 
         def __str__(self):
                 return f"{self.nom}({self.grpaddr}.{self.dpt} @ {self.candest} {self.unit} )"
@@ -35,7 +37,8 @@ class wpf:
         @async_to_sync
         async def setval(self,val):
 
-                global xknx
+#                global xknx
+
                 self.val = val
                 if __main__.debug: print('set ',self.nom, ' in knx group ',self.grpaddr, 'to ', val)
 #                encval = myknx.encode_dpt9(val)
@@ -45,5 +48,9 @@ class wpf:
 #                os.system(comd) 
 
 #
-                self.xknx.write(self.grpaddr,val,knx.encode_dt_temp)
+                try:
+                        self.xknx.write(self.grpaddr,val,knx.encode_dt_temp)
+                except:
+                        self.xknx = connect(self.knxgtw)
+                        self.xknx.write(self.grpaddr,val,knx.encode_dt_temp)
 
